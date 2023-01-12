7aa8e4bab9d2626ace6e9cd79d619fb87cf076ff
Saurav <kundusaurav1996@gmail.com>
2023-01-13 01:52:10 +0530

    pushe message

--- src/conponents/Sidebar.js
+++ src/conponents/Sidebar.js
@@ -1,20 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Sidebar() {
   return (
-    <div className='sidebar'>
-    <div className="nav">
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#resume">Resume</a>
-            <a href="#project">Projects</a>
-            <a href="#testimonial">Testimonial</a>
-            <a href="#contact">ContactMe</a>
-        </div>
-   
-
+    <div className="sidebar">
+      <div className="nav">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#resume">Resume</a>
+        <a href="#project">Projects</a>
+        <a href="#testimonial">Testimonial</a>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
